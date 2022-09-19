50435333155215baaad224d6bca56ce31c084900
Narendran Arumugavadivelu <narendran.arumugavadivelu@gmail.com>
2022-09-19 20:04:46 +0530

    added console log for debugging

--- server/index.js
+++ server/index.js
@@ -58,6 +58,8 @@ app.post('/values', async(req, res) => {
     redisClient.hset('values', index, 'Nothing Yet!');
     redisPublisher.publish('insert', index); 
     pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+    const values = await pgClient.query("SELECT * FROM values");
+    console.log(values.rows);
     res.send({
         working: true
     });
